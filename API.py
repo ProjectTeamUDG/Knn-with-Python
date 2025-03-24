a0eca8432af7f8ba6e6f35c73a998892d3df7ae7
JonathanCivrian <jmcivrian@gmail.com>
2025-03-24 01:14:12 -0600

    Try using Clave_Articulo

--- API.py
+++ API.py
@@ -2,35 +2,45 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import joblib
 import pandas as pd
-import uvicorn
+
+def load_data(path, extension):
+    if extension == ".csv":
+        return pd.read_csv(path+extension)
+    elif extension == ".xlsx":
+        return pd.read_excel(path+extension)
+    return "Extension no valida"
 
 # Cargar modelo y columnas
 model = joblib.load("Modelo_KNN5.pkl")
-columns = joblib.load("Columnas.pkl")
 
 app = FastAPI()
 
 class Modelo_KNN(BaseModel):
     CLAVE_ARTICULO: int
     cantidad: int
+    linea_articulo: int
     precio: float
 
 @app.post("/recomendar")
 def recomendar(data: Modelo_KNN):
-    article_id = data.CLAVE_ARTICULO
-
+    dictionary = load_data("Temporal", ".xlsx")
     # Obtener características del artículo de referencia
     article_features = pd.DataFrame({
     "TOTAL_ARTICULOS": [data.cantidad],
     "TOTAL_CLIENTES": [1],
-    "LINEA_ARTICULO_ID": [1],
+    "LINEA_ARTICULO_ID": [data.linea_articulo],
     "PRECIO": [data.precio]
     })
 
     # Buscar los artículos más cercanos
     distances, indices = model.kneighbors(article_features)
 
+    results = []
+    for index in enumerate(indices):
+        similar_article = dictionary.iloc[index]["CLAVE_ARTICULO"]
+        results.append(similar_article)
+    temporal = pd.DataFrame({"CLAVE_ARTICULO": [results]})
     return {
-        "recomendaciones": indices[0].tolist(),
+        "recomendaciones":temporal,
         "distancias": distances[0].tolist()
     }
