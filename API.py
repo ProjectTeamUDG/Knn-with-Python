bf11de9dba8b61d8e8a733d09c6ef28e5959c266
JonathanCivrian <jmcivrian@gmail.com>
2025-03-31 23:52:46 -0600

    Added knn nearest neighbors on working local API

--- API.py
+++ API.py
@@ -1,46 +1,44 @@
 from fastapi import FastAPI
+from openpyxl.xml.functions import tostring
 from pydantic import BaseModel
 import joblib
-import pandas as pd
+from pprint import pprint
 
-def load_data(path, extension):
-    if extension == ".csv":
-        return pd.read_csv(path+extension)
-    elif extension == ".xlsx":
-        return pd.read_excel(path+extension)
-    return "Extension no valida"
 
 # Cargar modelo y columnas
 model = joblib.load("Modelo_KNN5.pkl")
+modelData = joblib.load("Data.pkl")
 
 app = FastAPI()
 
-class Modelo_KNN(BaseModel):
+class ModeloKNN(BaseModel):
     CLAVE_ARTICULO: int
-    cantidad: int
-    linea_articulo: int
-    precio: float
 
 @app.post("/recomendar")
-def recomendar(data: Modelo_KNN):
-    dictionary = load_data("Temporal", ".xlsx")
-    # Obtener características del artículo de referencia
-    article_features = pd.DataFrame({
-    "TOTAL_ARTICULOS": [data.cantidad],
-    "TOTAL_CLIENTES": [1],
-    "LINEA_ARTICULO_ID": [data.linea_articulo],
-    "PRECIO": [data.precio]
-    })
+def recomendar(data: ModeloKNN):
+    article_id = data.CLAVE_ARTICULO
+    if article_id not in modelData["CLAVE_ARTICULO"].values:
+        print(f"Artículo {article_id} no encontrado en los datos.")
+        return
+
+        # Obtener características del artículo de referencia
+    article_features = modelData[modelData["CLAVE_ARTICULO"] == article_id][
+        ["TOTAL_ARTICULOS", "TOTAL_CLIENTES", "LINEA_ARTICULO_ID", "PRECIO"]]
 
     # Buscar los artículos más cercanos
     distances, indices = model.kneighbors(article_features)
 
-    results = []
-    for index in enumerate(indices):
-        similar_article = dictionary.iloc[index]["CLAVE_ARTICULO"]
-        results.append(similar_article)
-    temporal = pd.DataFrame({"CLAVE_ARTICULO": [results]})
+    print(f"Artículos similares a {article_id}:")
+
+    recommendations = []
+    for i, index in enumerate(indices[0]):
+        similar_article = modelData.iloc[index]["CLAVE_ARTICULO"]
+        if i > 0:
+            recommendations.append(similar_article)
+            print(f"{i}. Artículo {similar_article} (Distancia: {distances[0][i]:.4f})")
+
     return {
-        "recomendaciones":temporal,
+        "recomendaciones":recommendations,
         "distancias": distances[0].tolist()
     }
+
--- Gen_ModelKNN.py
+++ Gen_ModelKNN.py
@@ -2,7 +2,7 @@ import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.neighbors import NearestNeighbors
 import joblib
-
+import time
 
 def load_data(path, extension):
     if extension == ".csv":
@@ -18,12 +18,14 @@ def normalize(data):
 
 def train_knn(data):
     features = ["TOTAL_ARTICULOS", "TOTAL_CLIENTES", "LINEA_ARTICULO_ID", "PRECIO"]
-    knn = NearestNeighbors(n_neighbors=6, metric='euclidean')
+    knn = NearestNeighbors(n_neighbors=3, metric='euclidean', algorithm='auto')
     knn.fit(data[features])
     return knn
 
 
 def recommend(knn, data, article_id):
+
+
     if article_id not in data["CLAVE_ARTICULO"].values:
         print(f"Artículo {article_id} no encontrado en los datos.")
         return
@@ -43,19 +45,35 @@ def recommend(knn, data, article_id):
         if similar_article != article_id:
             print(f"{i}. Artículo {similar_article} (Distancia: {distances[0][i]:.4f})")
 
-def export_model(model_knn):
+def export_model(model_knn, data):
     joblib.dump(model_knn, "Modelo_KNN5.pkl")
-    print("Modelo exportado")
+    joblib.dump( data, "Data.pkl")
+    print("Modelo y data exportados")
 
 def main():
     data = load_data("Data", ".xlsx")
     data = normalize(data)
 
     knn = train_knn(data)
-    export_model(knn)
+    export_model(knn, data)
 
     # for i in range(10):
     #     CLAVE_ARICULO = int(input("Ingrese CLAVE_ARICULO:"))
     #     # Prueba recomendando artículos similares a uno dado (por ejemplo, el artículo con ID 12345)
     #     recommend(knn, data, 100002)
+    start = time.time()
+    recommend(knn, data, 328002)
+    end = time.time()
+
+    print(f"Tiempo de ejecución: {end - start:.4f} segundos")
+    start = time.time()
+    recommend(knn, data, 894186)
+    end = time.time()
+
+    print(f"Tiempo de ejecución: {end - start:.4f} segundos")
+    start = time.time()
+    recommend(knn, data, 100002)
+    end = time.time()
+
+    print(f"Tiempo de ejecución: {end - start:.4f} segundos")
 main()
